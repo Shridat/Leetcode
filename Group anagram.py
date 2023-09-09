317fb5800af77d915d70b370d2054482618830d1
Shrinivas Patil <80768804+Shridat@users.noreply.github.com>
2023-09-09 01:52:02 -0700

    Group anagram.py

---
+++ Group anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0]*26
+            for ch in s:
+                count[ord(ch)-ord('a')]+=1
+            ans[tuple(count)].append(s)
+        return ans.values()
+'''
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+'''
