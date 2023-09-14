4491dcb9417bf9db93d1acffbb0b5240c5ce5f0a
Shrinivas Patil <80768804+Shridat@users.noreply.github.com>
2023-09-14 16:59:59 -0700

    Min Stack.py

---
+++ min stack.py
@@ -0,0 +1,38 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack= []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+'''
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+'''
